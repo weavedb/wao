05c7eb9c90a5b9b1636b9692d558e28ff6f98f36
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2024-12-09 00:36:27 +0400

    make in-memory GQL fully compatible

--- src/tar.js
+++ src/tar.js
@@ -30,11 +30,13 @@ class AR extends MAR {
     let tx = await this.arweave.createTransaction({ data: data })
     let _tags = buildTags(null, tags)
     for (const v of _tags) tx.addTag(v.name, v.value)
+    const owner = await this.arweave.wallets.jwkToAddress(jwk)
+    this.mem.addrmap[owner] = jwk.n
     return await this.postTx(tx, jwk, {
       recipient: "",
       tags: _tags,
       data,
-      owner: await this.arweave.wallets.jwkToAddress(jwk),
+      owner,
     })
   }
 
@@ -43,16 +45,23 @@ class AR extends MAR {
     tx.addTag("Bundle-Format", "binary")
     tx.addTag("Bundle-Version", "2.0.0")
     const di = new DataItem(item.raw)
+    const data_size = Buffer.byteLength(di.rawData).toString()
+    let data_type = ""
+    for (const t of di.tags) if (t.name === "Content-Type") data_type = t.value
     const rowner = di.rawOwner
     const hashBuffer = Buffer.from(
       await crypto.subtle.digest("SHA-256", rowner),
     )
     const owner = base64url.encode(hashBuffer)
+    this.mem.addrmap[owner] = di.owner
     let data = di.data
     try {
       data = base64url.decode(di.data)
     } catch (e) {}
     let _item = {
+      _data: { size: data_size, type: data_type },
+      anchor: di.anchor,
+      signature: di.signature,
       recipient: di.target,
       id: item.id,
       item: di,
@@ -69,28 +78,38 @@ class AR extends MAR {
     await this.mem.arweave.transactions.sign(tx, jwk)
     if (item) {
       this.mem.height += 1
-
       const block = {
         id: tx.id,
         timestamp: Date.now(),
         height: this.mem.height,
         previous: last(this.mem.blocks) ?? "",
       }
-
       if (!item.id) {
         item.id = tx.id
         this.mem.txs[item.id] = item
-        this.mem.txs[item.id].parent = { id: block.id }
+        this.mem.txs[item.id].parent = null
+        this.mem.txs[item.id].signature = tx.signature
+        this.mem.txs[item.id].anchor = tx.last_tx
+        let data_type = ""
+        for (const v of tx.tags) {
+          if (
+            v.get("name", { decode: true, string: true }) === "Content-Type"
+          ) {
+            data_type = v.get("value", { decode: true, string: true })
+          }
+        }
+        this.mem.txs[item.id]._data = { size: tx.data_size, type: data_type }
       } else {
-        this.mem.txs[item.id].parent = { id: "" }
+        this.mem.txs[item.id].parent = { id: block.id }
       }
       block.txs = [item.id]
       this.mem.txs[item.id].block = block.id
-      this.mem.txs[item.id].parent = { id: block.id }
       this.mem.blocks.push(block.id)
       this.mem.blockmap[block.id] = block
     }
     res = { id: tx.id, status: 200, statusText: "200" }
+    const owner = await this.arweave.wallets.jwkToAddress(jwk)
+    this.mem.addrmap[owner] = jwk.n
     return { res, err, id: tx.id }
   }
 
