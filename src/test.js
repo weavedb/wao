e7df1ef8488da87911efa3474a0692fae44446c8
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2024-12-09 23:08:12 +0400

    fix misc

--- src/test.js
+++ src/test.js
@@ -5,10 +5,24 @@ import { connect } from "./aoconnect.js"
 import AO from "./tao.js"
 import AR from "./tar.js"
 import { dirname } from "./utils.js"
-
+import { Src, setup, ok, fail } from "./helpers.js"
 const blueprint = async pkg => {
   return readFileSync(resolve(await dirname(), `lua/${pkg}.lua`), "utf8")
 }
 const scheduler = "GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA"
 
-export { AO, AR, connect, acc, mu, su, cu, blueprint, scheduler }
+export {
+  AO,
+  AR,
+  connect,
+  acc,
+  mu,
+  su,
+  cu,
+  blueprint,
+  scheduler,
+  ok,
+  fail,
+  Src,
+  setup,
+}
