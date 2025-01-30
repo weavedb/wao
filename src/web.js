a7b5b37e74734b58f7c241022722970e5f11c7dc
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-01-30 18:12:05 +0400

    web embeddable WAO

---
+++ src/web.js
@@ -0,0 +1,11 @@
+import { acc, mu, su, cu } from "./accounts-web.js"
+import { connect } from "./aoconnect-web.js"
+import AO from "./wao.js"
+import AR from "./war.js"
+import GQL from "./tgql.js"
+import ArMem from "./armem-web.js"
+import { dirname } from "./utils.js"
+
+const scheduler = "_GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA"
+
+export { GQL, ArMem, AO, AR, connect, acc, mu, su, cu, scheduler }
