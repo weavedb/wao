3fd2ce51ddb97074c42da6d43e3871cecc7037a7
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-02-01 08:23:10 +0400

    make compressor compatible with nodejs

--- src/armem-web.js
+++ src/armem-web.js
@@ -1,10 +1,11 @@
 import wdb from "./lua/weavedb-lite.js"
 import Base from "./armem-base.js"
 import db from "./lfdb.js"
+import init, { Compressor, Decompressor } from "./waosm/waosm.js"
 
 export default class ArMem extends Base {
   constructor(args = {}) {
-    super(args)
+    super({ ...args, init, Compressor, Decompressor })
     this.db = db(this)
     this.initSync()
   }
