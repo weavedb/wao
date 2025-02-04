da639541f0f4be2af36ab8111ff0fd8531d07eee
Tomoya Nagasawa <ocrybit@gmail.com>
2025-02-04 12:44:14 +0400

    embed LLMs in the browser

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,8 @@
 const arnext = require("arnext/config")
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+}
 module.exports = arnext(nextConfig)
