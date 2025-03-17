719eeb9ef0aadc4c6fcd94c563856675cdb753ce
Tomoya Nagasawa <ocrybit@gmail.com>
2025-03-17 12:46:45 +0400

    fix: cjs dirname import

--- src/waosm-node.js
+++ src/waosm-node.js
@@ -3,11 +3,23 @@ import * as wasmBindings from "./waosm-node/waosm_bg.js"
 import { __wbg_set_wasm } from "./waosm-node/waosm_bg.js"
 import { readFileSync } from "fs"
 import { resolve } from "path"
-const wasm = readFileSync(
-  resolve(import.meta.dirname, "./waosm-node/waosm_bg.wasm"),
-)
-const wasmModule = new WebAssembly.Module(wasm)
-const wasmInstance = new WebAssembly.Instance(wasmModule, {
-  "./waosm_bg.js": wasmBindings,
-})
-__wbg_set_wasm(wasmInstance.exports)
+
+const run = dirname => {
+  const wasm = readFileSync(resolve(dirname, "./waosm-node/waosm_bg.wasm"))
+  const wasmModule = new WebAssembly.Module(wasm)
+  const wasmInstance = new WebAssembly.Instance(wasmModule, {
+    "./waosm_bg.js": wasmBindings,
+  })
+  __wbg_set_wasm(wasmInstance.exports)
+}
+
+let dirname
+if (typeof __filename !== "undefined" && typeof __dirname !== "undefined")
+  run(__dirname)
+else {
+  import("./dirname.js")
+    .then(({ default: dirname }) => run(dirname))
+    .catch(e => {
+      console.log(e)
+    })
+}
