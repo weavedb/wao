ef382e667dee6c3b939b05592faf579875a7adb2
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-04-15 10:49:42 +0400

    [app] refactor #1

--- app/components/Xterm.js
+++ app/components/Xterm.js
@@ -3,42 +3,8 @@ import chalk from "chalk"
 import { Box } from "@chakra-ui/react"
 import { Terminal } from "@xterm/xterm"
 import "@xterm/xterm/css/xterm.css"
-import { FitAddon } from "./addon-fit"
-const Style = () => (
-  <style jsx global>{`
-    /* Custom scrollbar for terminal */
-    #terminal .xterm-viewport::-webkit-scrollbar {
-      width: 10px;
-      background-color: #111;
-    }
-
-    #terminal .xterm-viewport::-webkit-scrollbar-thumb {
-      background-color: #444;
-      border-radius: 4px;
-    }
-
-    #terminal .xterm-viewport::-webkit-scrollbar-track {
-      background-color: #222;
-    }
-
-    #terminal .xterm-viewport {
-      scrollbar-width: thin;
-      scrollbar-color: #444 #222;
-    }
-
-    /* Make terminal use full available height */
-    #terminal .xterm {
-      height: 200px !important;
-    }
-
-    #terminal .xterm-screen {
-      height: 100% !important;
-    }
-    #terminal .xterm-rows {
-      padding: 5px 10px;
-    }
-  `}</style>
-)
+import { FitAddon } from "../lib/addon-fit"
+import XtermStyle from "./XtermStyle"
 const config = {
   cursorBlink: true,
   fontSize: 12,
@@ -303,5 +269,5 @@ export default function XTerm({ global: g }) {
     }
   }, [])
 
-  return <Style />
+  return <XtermStyle />
 }
