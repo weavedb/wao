97dcb4808343d37623b20bd871a75ea0b6b4a932
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-04-19 09:51:26 +0400

    [app] account page

--- app/components/middle/MiddleEntity.js
+++ app/components/middle/MiddleEntity.js
@@ -2,8 +2,9 @@ import use from "/lib/use"
 import Module from "/components/middle/MiddleEntityModule"
 import Process from "/components/middle/MiddleEntityProcess"
 import Message from "/components/middle/MiddleEntityMessage"
+import Account from "/components/middle/MiddleEntityAccount"
 
-const components = { Module, Process, Message }
+const components = { Module, Process, Message, Account }
 
 export default function MiddleEntity() {
   const [entity] = use("entity")
