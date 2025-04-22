58e690f53611d012f7c967392661168e39cb8909
Tomoya Nagasawa <ocrybit@gmail.com>
2025-04-22 10:05:09 +0400

    [app] disable buttons when not available

--- app/components/left/LeftTests.js
+++ app/components/left/LeftTests.js
@@ -13,6 +13,7 @@ export default function Left() {
   const [file, setFile] = use("file")
   const [proc, setProc] = use("proc")
   const [tab, setTab] = use("tab")
+  const isTest = file?.ext === "js" && proc
   const buttons = (
     <Flex
       h="60px"
@@ -21,117 +22,119 @@ export default function Left() {
       fontSize="12px"
       css={{ borderBottom: "1px solid #ddd" }}
     >
-      {!file || file.ext !== "js" ? null : (
-        <>
-          <Flex
-            py={1}
-            px={3}
-            fontSize="10px"
-            color="#ddd"
-            bg="#5137C5"
-            css={{
-              borderRadius: "5px",
-              cursor: "pointer",
-              _hover: { opacity: 0.75 },
-            }}
-            onClick={async () => {
-              try {
-                const js = g.editorRef.current.getValue()
-                const p = proc ? g.ao.p(proc.id) : null
-                let descs = []
-                const src = async path => {
-                  for (let v of files) {
-                    if (v.name === path) {
-                      return await lf.getItem(`file-${v.id}`)
-                    }
+      <>
+        <Flex
+          py={1}
+          px={3}
+          fontSize="10px"
+          color="#ddd"
+          bg={isTest ? "#5137C5" : "#999"}
+          css={{
+            borderRadius: "5px",
+            cursor: "pointer",
+            _hover: { opacity: 0.75 },
+          }}
+          onClick={async () => {
+            if (!isTest)
+              return file?.ext !== "js"
+                ? alert("Select a JS file.")
+                : alert("Connect with a process.")
+            try {
+              const js = g.editorRef.current.getValue()
+              const p = proc ? g.ao.p(proc.id) : null
+              let descs = []
+              const src = async path => {
+                for (let v of files) {
+                  if (v.name === path) {
+                    return await lf.getItem(`file-${v.id}`)
                   }
-                  return null
                 }
-                const assert = _assert
-                const require = async name => {
-                  let _module = { exports: null }
-                  const js = await src(name)
-                  eval(js)
-                  return _module.exports
-                }
-                let i = 0
-                const it = (desc, fn) => {
-                  descs[i].tests.push({ desc, fn })
-                }
-
-                const describe = (desc2, fn) => {
-                  descs.push({ desc: desc2, fn, tests: [] })
-                }
-                eval(js)
-                const ts = Date.now()
-                let success = 0
-                let fail = 0
-                let res = []
-                for (let v of descs) {
-                  let _res = []
-                  let _success = 0
-                  let _fail = 0
-                  await v.fn({ require })
-                  for (let v2 of v.tests) {
-                    const start = Date.now()
-                    try {
-                      await v2.fn({
-                        ao: g.ao,
-                        src,
-                        p,
-                      })
-                      _res.push({
-                        description: v2.desc,
-                        success: true,
-                        error: null,
-                        duration: Date.now() - start,
-                      })
-                      _success++
-                      success++
-                    } catch (e) {
-                      _res.push({
-                        description: v2.desc,
-                        success: false,
-                        error: e.toString(),
-                        duration: Date.now() - start,
-                      })
-                      _fail++
-                      fail++
-                    }
-                  }
-                  res.push({
-                    description: v.desc,
-                    cases: _res,
-                    success: _success,
-                    fail: _fail,
-                  })
-                  i++
-                }
-                const result = {
-                  file: file.name,
-                  process: proc?.id ?? null,
-                  id: generateId(),
-                  date: ts,
-                  duration: Date.now() - ts,
-                  tests: res,
-                  success,
-                  fail,
-                }
-                if (success > 0 || fail > 0) {
-                  setTab("Tests")
-                  setTest(result)
-                  setTests([result, ...tests])
-                }
-              } catch (e) {
-                console.log(e)
+                return null
               }
-            }}
-          >
-            Run Test
-          </Flex>
-          <Box flex={1} />
-        </>
-      )}
+              const assert = _assert
+              const require = async name => {
+                let _module = { exports: null }
+                const js = await src(name)
+                eval(js)
+                return _module.exports
+              }
+              let i = 0
+              const it = (desc, fn) => {
+                descs[i].tests.push({ desc, fn })
+              }
+
+              const describe = (desc2, fn) => {
+                descs.push({ desc: desc2, fn, tests: [] })
+              }
+              eval(js)
+              const ts = Date.now()
+              let success = 0
+              let fail = 0
+              let res = []
+              for (let v of descs) {
+                let _res = []
+                let _success = 0
+                let _fail = 0
+                await v.fn({ require })
+                for (let v2 of v.tests) {
+                  const start = Date.now()
+                  try {
+                    await v2.fn({
+                      ao: g.ao,
+                      src,
+                      p,
+                    })
+                    _res.push({
+                      description: v2.desc,
+                      success: true,
+                      error: null,
+                      duration: Date.now() - start,
+                    })
+                    _success++
+                    success++
+                  } catch (e) {
+                    _res.push({
+                      description: v2.desc,
+                      success: false,
+                      error: e.toString(),
+                      duration: Date.now() - start,
+                    })
+                    _fail++
+                    fail++
+                  }
+                }
+                res.push({
+                  description: v.desc,
+                  cases: _res,
+                  success: _success,
+                  fail: _fail,
+                })
+                i++
+              }
+              const result = {
+                file: file.name,
+                process: proc?.id ?? null,
+                id: generateId(),
+                date: ts,
+                duration: Date.now() - ts,
+                tests: res,
+                success,
+                fail,
+              }
+              if (success > 0 || fail > 0) {
+                setTab("Tests")
+                setTest(result)
+                setTests([result, ...tests])
+              }
+            } catch (e) {
+              console.log(e)
+            }
+          }}
+        >
+          Run Test
+        </Flex>
+        <Box flex={1} />
+      </>
     </Flex>
   )
   return (
