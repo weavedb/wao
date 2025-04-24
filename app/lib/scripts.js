b9f257adf4b7b6f2688d123711a5e59acbf331f8
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-04-25 02:00:53 +0400

    [app] update default scripts

--- app/lib/scripts.js
+++ app/lib/scripts.js
@@ -1,10 +1,45 @@
-const src_data_js = `describe("WAO", ()=>{
+const src_data_js = `const src_data = \`local count = 0
+
+Handlers.add("Inc", "Inc", function (msg)
+  count = count + 1
+  msg.reply({ Data = "Incremented!" })
+end)
+
+Handlers.add("Get", "Get", function (msg)
+  msg.reply({ Data = tostring(count) })
+end)
+
+Handlers.add("Hello", "Hello", function (msg)
+  msg.reply({ Data = "Hello, World!" })
+end)\`
+
+describe("Counter", ()=>{
+  it("should increment", async ({ ao })=> {
+    const { p } = await ao.deploy({src_data})
+    assert.equal(await p.d("Hello", false), "Hello, World!")
+    assert.equal(await p.d("Get"), "0")
+    assert.equal(await p.m("Inc", false), "Incremented!")
+    assert.equal(await p.d("Get"), "1")
+  })
+})
+
+describe("WAO", ()=>{
   it("should run", async ({ ao, p, src })=> {
-    // write your test here
+    // write your test
   })
 })`
 
-const src_data_lua = `
+const src_data_lua = `local count = 0
+
+Handlers.add("Inc", "Inc", function (msg)
+  count = count + 1
+  msg.reply({ Data = "Incremented!" })
+end)
+
+Handlers.add("Get", "Get", function (msg)
+  msg.reply({ Data = tostring(count) })
+end)
+
 Handlers.add("Hello", "Hello", function (msg)
   msg.reply({ Data = "Hello, World!" })
 end)`
