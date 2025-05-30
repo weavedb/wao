4d47698c8273006664dd2d2527ee3996fff249b1
Tomoya Nagasawa <ocrybit@gmail.com>
2025-05-30 18:04:30 +0400

    standalone http-message signer/verifier

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 import { graphql, parse, validate, buildSchema } from "graphql"
-
+import sha256 from "fast-sha256"
 import {
   clone,
   is,
@@ -701,6 +701,31 @@ function parseSignatureInput(input) {
   return { label, fields, alg, keyid }
 }
 
+function base64urlDecode(str) {
+  str = str.replace(/-/g, "+").replace(/_/g, "/")
+  const pad = str.length % 4
+  if (pad === 2) str += "=="
+  else if (pad === 3) str += "="
+  else if (pad !== 0) throw new Error("Invalid base64url string")
+  const bin = atob(str)
+  const bytes = new Uint8Array(bin.length)
+  for (let i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i)
+  return bytes
+}
+
+function base64urlEncode(bytes) {
+  let bin = ""
+  for (const b of bytes) bin += String.fromCharCode(b)
+  let b64 = btoa(bin)
+  return b64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/g, "")
+}
+
+function toAddr(n) {
+  const pubBytes = base64urlDecode(n)
+  const hash = sha256(pubBytes)
+  return base64urlEncode(hash)
+}
+
 export {
   toANS104Request,
   parseSignatureInput,
@@ -732,4 +757,5 @@ export {
   udl,
   isJSON,
   dirname,
+  toAddr,
 }
