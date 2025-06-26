bfa65118649dd41be31199e0f180db97f6c7b642
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-06-26 08:17:44 +0400

    test patch@1.0

---
+++ test/patch.test.js
@@ -0,0 +1,78 @@
+import assert from "assert"
+import { after, describe, it, before, beforeEach } from "node:test"
+import { acc, toAddr } from "../src/test.js"
+import { getJWK } from "./lib/test-utils.js"
+import HB from "../src/hb.js"
+import { pick } from "ramda"
+import { wait } from "../src/utils.js"
+import Server from "../src/server.js"
+import HyperBEAM from "../src/hyperbeam.js"
+import { extractPublicKeyFromHeaders } from "../src/signer.js"
+const seed = num => {
+  const array = new Uint8Array(num)
+  return crypto.getRandomValues(array).toString()
+}
+
+const URL = "http://localhost:10001"
+
+const data = `
+local count = 0
+Handlers.add("Inc", "Inc", function (msg)
+  count = count + 1
+  msg.reply({ Data = "Count: "..tostring(count) })
+end)
+
+Handlers.add("Get", "Get", function (msg)
+  msg.reply({ Data = "Count: "..tostring(count) })
+end)`
+
+const src_data = `
+local count = 0
+Handlers.add("Add", "Add", function (msg)
+  count = count + tonumber(msg.Plus)
+end)
+
+Handlers.add("Get", "Get", function (msg)
+  msg.reply({ Data = tostring(count) })
+end)
+`
+
+describe("Hyperbeam Device", function () {
+  let hb, hb2, hbeam, jwk, server, addr, addr2
+  before(async () => {
+    server = new Server({ port: 6359, log: true, hb_url: URL })
+    jwk = getJWK("../../HyperBEAM/.wallet.json")
+    addr = toAddr(jwk.n)
+    addr2 = toAddr(acc[0].jwk.n)
+    hbeam = new HyperBEAM({
+      clearCache: true,
+      gateway: 6359,
+      c: "12",
+      cmake: "3.5",
+      operator: addr,
+    })
+    await wait(5000)
+  })
+
+  beforeEach(async () => {
+    hb = await new HB({}).init(jwk)
+    hb2 = await new HB({}).init(acc[0].jwk)
+  })
+
+  after(async () => hbeam.kill())
+
+  it.only("should test patch@1.0", async () => {
+    const { pid } = await hb.spawn({
+      "execution-device": "stack@1.0",
+      "device-stack": ["wao@1.0", "patch@1.0"],
+      "patch-from": "/results",
+      "patch-to": "/cache",
+    })
+    await hb.schedule({ pid })
+    await hb.schedule({ pid })
+    const square = (await hb.now({ pid, path: "/cache/square" })).body
+    const double = (await hb.now({ pid, path: "/cache/double" })).body
+    assert.equal(square, 9)
+    assert.equal(double, 6)
+  })
+})
