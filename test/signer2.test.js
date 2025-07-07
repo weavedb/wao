93148c8c536519e78fe45bc3717d5bd340227817
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2025-07-07 07:45:56 +0400

    [signer] solve error cases

--- test/signer2.test.js
+++ test/signer2.test.js
@@ -20,6 +20,7 @@ describe("Hyperbeam Signer", function () {
     //let cases = generateTestCases(1000)
     let cases = keys
     let err = []
+    let success = []
     for (const v of cases) {
       const msg = await hb.sign({ path: "/~wao@1.0/httpsig", ...v })
       console.log(msg)
@@ -28,12 +29,13 @@ describe("Hyperbeam Signer", function () {
         const json = JSON.parse(body)
         const transformed = mod(v)
         assert.deepEqual(json, transformed)
+        success.push(v)
       } catch (e) {
         console.log(e)
         err.push(v)
       }
     }
-    console.log(`${err.length} failed!`)
+    console.log(`${err.length} / ${cases.length} failed!`)
     if (err) {
       for (let v of err) console.log(v)
     }
