c825d21392d86409bb98c81ead82ae16afab8e86
Tomoya Nagasawa <ocrybit@gmail.com>
2025-07-29 03:21:12 +0400

    [hbsig] fix commit

--- hbsig/test/commit.test.js
+++ hbsig/test/commit.test.js
@@ -8,20 +8,33 @@ import { createSigner } from "../src/signer.js"
 import { send } from "../src/send.js"
 import { commit } from "../src/commit.js"
 
-describe("Hyperbeam Signer", function () {
-  let hbeam, sign
+describe("Hyperbeam commit", function () {
+  let hbeam, sign, hb
   before(async () => {
     hbeam = await new HyperBEAM({ reset: true }).ready()
     sign = createSigner(hbeam.jwk, hbeam.url)
+    hb = hbeam.hb
   })
   after(async () => hbeam.kill())
 
-  it("should test signer", async () => {
-    const msg = await commit({ key: "value" }, { signer: sign })
+  it("should test commit", async () => {
+    const msg = await commit(
+      { key: "value", data: Buffer.from([1, 2, 3]) },
+      { signer: sign }
+    )
     for (const k in msg.commitments) {
       if (msg.commitments[k].committer) {
         assert.equal(hbeam.addr, msg.commitments[k].committer)
       }
     }
+  })
+  it("should schedule a nested message", async () => {
+    const { pid } = await hb.spawn()
+    const { slot } = await hb.schedule({
+      pid,
+      tags: { str: "value", num: 123 },
+      data: "abc",
+    })
+    assert.equal(1, slot)
   })
 })
