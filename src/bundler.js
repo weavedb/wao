d529ae474a615aff2d2d43bc4972fcaa7a557b2c
Tomoya Nagasawa <ocrybit@gmail.com>
2025-09-27 22:31:08 +0400

    [bundler_httpsig] filter non-ans104 messages

--- src/bundler.js
+++ src/bundler.js
@@ -31,80 +31,82 @@ const bundler = ({ port = 4001, jwk, timeout = 1000 * 60 } = {}) => {
       const out = structured_to(httpsig_from(toMsg(req)))
       for (const k in out.body?.commitments ?? {}) {
         const msg = out.body?.commitments[k]
-        const signer = {
-          signatureType: 1,
-          signatureLength: 512,
-          ownerLength: 512,
-          publicKey: msg.owner,
-          sign: async () => {},
-        }
-        const tags = []
-        let type = null
-        if (msg["original-tags"]) {
-          for (const tagObj of Object.values(msg["original-tags"])) {
-            tags.push({ name: tagObj.name, value: tagObj.value })
-            if (tagObj.name === "Type") {
-              type = tagObj.value
-            }
-          }
-        }
-        let opt = { tags }
-        if (type !== "Process") opt.target = out.process
-        const di = await createData(out.data || "", signer, opt)
-        await di.setSignature(msg.signature)
-        const res = await fetch("https://up.arweave.net:443/tx", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/octet-stream",
-          },
-          body: di.binary,
-        })
-        console.log(res?.status, di.tags)
-        const dis = [di]
-        if (type === "Message") {
-          if (!out["block-timestamp"]) {
-            try {
-              if (Date.now() - last_checked > timeout) {
-                const json = await fetch(
-                  "https://arweave.net/block/current"
-                ).then(r => r.json())
-                height = json.height
-                timestamp = json.timestamp
-                last_checked = Date.now()
-              }
-            } catch (e) {
-              console.log(e)
-            }
-          }
-          const atags = {
-            Process: out.process,
-            Epoch: out.epoch,
-            Nonce: Number(++nonce).toString(),
-            "Hash-Chain": out["hash-chain"],
-            "Block-Height": Number(out["block-height"] ?? height).toString(),
-            Tmestamp: Number(out["block-timestamp"] ?? timestamp).toString(),
-            "Data-Protocol": "ao",
-            Slot: Number(out.slot).toString(),
-            Variant: "ao.WDB.1",
-            Type: "Assignment",
-            Message: di.id,
+        if (msg["commitment-device"] === "ans104@1.0") {
+          const signer = {
+            signatureType: 1,
+            signatureLength: 512,
+            ownerLength: 512,
+            publicKey: msg.owner,
+            sign: async () => {},
           }
           const tags = []
+          let type = null
           if (msg["original-tags"]) {
-            for (const k in atags) tags.push({ name: k, value: atags[k] })
+            for (const tagObj of Object.values(msg["original-tags"])) {
+              tags.push({ name: tagObj.name, value: tagObj.value })
+              if (tagObj.name === "Type") {
+                type = tagObj.value
+              }
+            }
           }
-          const signer = new ArweaveSigner(jwk)
-          const di2 = await createData("", signer, { tags })
-          await di2.sign(signer)
-          dis.push(di2)
-          const res2 = await fetch("https://up.arweave.net:443/tx", {
+          let opt = { tags }
+          if (type !== "Process") opt.target = out.process
+          const di = await createData(out.data || "", signer, opt)
+          await di.setSignature(msg.signature)
+          const res = await fetch("https://up.arweave.net:443/tx", {
             method: "POST",
             headers: {
               "Content-Type": "application/octet-stream",
             },
-            body: di2.binary,
+            body: di.binary,
           })
-          console.log(res2?.status, di2.tags)
+          console.log(res?.status, di.tags)
+          const dis = [di]
+          if (type === "Message") {
+            if (!out["block-timestamp"]) {
+              try {
+                if (Date.now() - last_checked > timeout) {
+                  const json = await fetch(
+                    "https://arweave.net/block/current"
+                  ).then(r => r.json())
+                  height = json.height
+                  timestamp = json.timestamp
+                  last_checked = Date.now()
+                }
+              } catch (e) {
+                console.log(e)
+              }
+            }
+            const atags = {
+              Process: out.process,
+              Epoch: out.epoch,
+              Nonce: Number(++nonce).toString(),
+              "Hash-Chain": out["hash-chain"],
+              "Block-Height": Number(out["block-height"] ?? height).toString(),
+              Tmestamp: Number(out["block-timestamp"] ?? timestamp).toString(),
+              "Data-Protocol": "ao",
+              Slot: Number(out.slot).toString(),
+              Variant: "ao.WDB.1",
+              Type: "Assignment",
+              Message: di.id,
+            }
+            const tags = []
+            if (msg["original-tags"]) {
+              for (const k in atags) tags.push({ name: k, value: atags[k] })
+            }
+            const signer = new ArweaveSigner(jwk)
+            const di2 = await createData("", signer, { tags })
+            await di2.sign(signer)
+            dis.push(di2)
+            const res2 = await fetch("https://up.arweave.net:443/tx", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/octet-stream",
+              },
+              body: di2.binary,
+            })
+            console.log(res2?.status, di2.tags)
+          }
         }
       }
     } catch (e) {
