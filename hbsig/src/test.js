279b23ebb55e82f63352134bbf283496f0cb04f3
Tomoya Nagasawa <40726926+tomoio@users.noreply.github.com>
2026-01-23 00:07:43 +0400

    [hbsig] fix missing imports

--- hbsig/src/test.js
+++ hbsig/src/test.js
@@ -1,6 +1,2 @@
-import { readFileSync } from "fs"
-import { resolve } from "path"
-import { acc } from "./accounts.js"
-import { toAddr, dirname, wait } from "./utils.js"
-
-export { toAddr, wait, acc }
+import { toAddr } from "./utils.js"
+export { toAddr }
