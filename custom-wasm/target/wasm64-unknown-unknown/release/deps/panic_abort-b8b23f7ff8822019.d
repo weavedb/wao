c041578279200d5ea8b4baf13b23187582ddd4a8
Tomoya Nagasawa <ocrybit@gmail.com>
2026-02-16 18:58:46 +0400

    Remove tutorial-generated directories (custom-lua, custom-wasm, dev_gleam_counter)

--- custom-wasm/target/wasm64-unknown-unknown/release/deps/panic_abort-b8b23f7ff8822019.d
+++
@@ -1,7 +0,0 @@
-/home/basque/oasis/wao-beta3/custom-wasm/target/wasm64-unknown-unknown/release/deps/libpanic_abort-b8b23f7ff8822019.rmeta: /home/basque/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
-
-/home/basque/oasis/wao-beta3/custom-wasm/target/wasm64-unknown-unknown/release/deps/libpanic_abort-b8b23f7ff8822019.rlib: /home/basque/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
-
-/home/basque/oasis/wao-beta3/custom-wasm/target/wasm64-unknown-unknown/release/deps/panic_abort-b8b23f7ff8822019.d: /home/basque/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
-
-/home/basque/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs:
